4ef4eff14498f3a3195cd5e288b6b6510b1bca0c
Min Li <lim83@cardiff.ac.uk>
2023-01-13 09:06:16 +0000

    Update __init__.py

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 # from flask_bootstrap import Bootstrap4
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_ckeditor import CKEditor
-from flask_mail import Mail
+# from flask_ckeditor import CKEditor
+# from flask_mail import Mail
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
@@ -21,12 +21,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c22094835:Li12345678@cs
 UPLOAD_FOLDER = './blog/static/img/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-ckeditor = CKEditor(app)
+# ckeditor = CKEditor(app)
 db = SQLAlchemy(app)
 # Bootstrap4(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-mail=Mail(app)
+# mail=Mail(app)
 # app.config['MAIL_SERVER']='smtp.gmail.com'
 # app.config['MAIL_PORT']=587
 # app.config['MAIL_USE_TLS']= True
